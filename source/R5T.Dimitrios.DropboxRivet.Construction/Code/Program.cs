3e431287c62fb62bbca456e961b1f11cbced476e
MinexAutomation <minex.automation@gmail.com>
2020-01-21 14:50:24 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Dimitrios.DropboxRivet.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Dimitrios.DropboxRivet.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
