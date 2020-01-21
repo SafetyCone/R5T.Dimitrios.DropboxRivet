aa01f0928061eae4fde045e51bdf56ed8fc2ad5a
MinexAutomation <minex.automation@gmail.com>
2020-01-21 15:02:07 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Dimitrios.DropboxRivet/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dimitrios.Maoursi;
+using R5T.Egaleo.Standard;
+using R5T.Lombardy.Standard;
+using R5T.Maoursi.Dropbox;
+using R5T.Oxford.Rivet.Standard;
+
+
+namespace R5T.Dimitrios.DropboxRivet
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDropboxRivetOrganizationDirectoryPathProvider(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<IOrganizationDirectoryPathProvider, OrganizationDirectoryPathProvider>()
+                .AddOrganizationDirectoryNameConvention()
+                .AddStringlyTypedPathOperator()
+                .AddDropboxOrganizationsDirectoryPathProvider()
+                .AddRivetOrganizationProvider()
+                ;
+
+            return services;
+        }
+    }
+}
